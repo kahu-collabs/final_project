c9f7b52cbe7c60a03be27350c740c7a5b43af40c
Petty Crime <email": null>
2015-10-09 17:57:08 +1300

    feature unblocked but still incomplete

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 var makeObjects = require('./source/map/make_objects')
 var getCrime = require('./source/map/getCrimeObject')
 var testType = require('./source/map/testType')
-// var filter = require('./source/map/filter')
+var filter = require('./source/map/filter')
 
 L.mapbox.accessToken = 'pk.eyJ1IjoicGV0dHljcmltZSIsImEiOiJjaWY0cTBoZDgwbXl0c2RtN2ZjYzhicjZoIn0.FDjxXktw-rA-U-qobjyNxQ';
 var map = L.mapbox.map(document.getElementById('map'), 'mapbox.streets')
@@ -21,12 +21,11 @@ function dat_get(){
 		  $( ".result" ).html( data );
 		  var renderObjects = makeObjects(data)
 		  render(renderObjects);
+      filter(map, myLayer)
 	});
 }
 
 function render(data){
-  myLayer.on('layeradd', function() {
-  });
   myLayer.setGeoJSON(data);
 }
 
@@ -44,7 +43,7 @@ function submitCrime(input){
 
 $(document).ready(function(){
     dat_get()
-    // filter(map, myLayer)
+
 })
 
 $('#example').submit(function(event){
@@ -54,59 +53,4 @@ $('#example').submit(function(event){
 	submitCrime(to_db);
 	dat_get();
 })
-
-// the great divide
-
-
-myLayer.on('layeradd', function() {
-  console.log("eatmyshorts")
-  // Collect the types of symbols in this layer. you can also just
-  // hardcode an array of types if you know what you want to filter on,
-  // like var types = ['foo', 'bar'];
-  var typesObj = {}, types = [];
-  console.log(myLayer)
-  var features = myLayer.getGeoJSON().features;
-  console.log(features)
-  for (var i = 0; i < features.length; i++) {
-    typesObj[features[i].properties['marker-symbol']] = true;
-  }
-  for (var k in typesObj) types.push(k);
-
-  var checkboxes = [];
-  // Create a filter interface.
-  for (var i = 0; i < types.length; i++) {
-    // Create an an input checkbox and label inside.
-    var item = filters.appendChild(document.createElement('div'));
-    var checkbox = item.appendChild(document.createElement('input'));
-    var label = item.appendChild(document.createElement('label'));
-    checkbox.type = 'checkbox';
-    checkbox.id = types[i];
-    checkbox.checked = true;
-    // create a label to the right of the checkbox with explanatory text
-    label.innerHTML = types[i];
-    label.setAttribute('for', types[i]);
-    // Whenever a person clicks on this checkbox, call the update().
-    checkbox.addEventListener('change', update);
-    checkboxes.push(checkbox);
-  }
-
-  // This function is called whenever someone clicks on a checkbox and changes
-  // the selection of markers to be displayed.
-  function update() {
-    var enabled = {};
-    // Run through each checkbox and record whether it is checked. If it is,
-    // add it to the object of types to display, otherwise do not.
-    for (var i = 0; i < checkboxes.length; i++) {
-      if (checkboxes[i].checked) enabled[checkboxes[i].id] = true;
-    }
-    map.featureLayer.setFilter(function(feature) {
-      // If this symbol is in the list, return true. if not, return false.
-      // The 'in' operator in javascript does exactly that: given a string
-      // or number, it says if that is in a object.
-      // 2 in { 2: true } // true
-      // 2 in { } // false
-      return (feature.properties['marker-symbol'] in enabled);
-    });
-  }
-});
 
