36f8b324d2c28bb24242b19c9259fb4e8ecc5ec4
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2015-10-12 17:18:19 +1300

    Add report method + tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151007225321) do
+ActiveRecord::Schema.define(version: 20151012035725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 20151007225321) do
     t.integer  "user_id"
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
+    t.datetime "time_1"
+    t.datetime "time_2"
   end
 
   add_index "reports", ["suburb_id"], name: "index_reports_on_suburb_id", using: :btree
--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe Report, type: :model do
     expect(invalid_report).to be_invalid
   end
 
-  xit "is invalid without date" do
+  it "is invalid without date" do
     invalid_report = build(:report, date: nil)
     expect(invalid_report).to be_invalid
   end
@@ -34,5 +34,29 @@ RSpec.describe Report, type: :model do
   it "report belongs to user" do
     report = Report.reflect_on_association(:user)
     expect(report.macro).to eq(:belongs_to)
+  end
+
+  it "has valid time row" do
+    report = build(:report, time_1: Time.now)
+    expect(report.time_1).to be_truthy
+  end
+
+  it "has valid datetime one" do
+    time = 2.hours.ago
+    report = build(:report, time_1: time)
+    expect(report.time_1).to eq(time)
+  end
+
+  it "has valid datetime two" do
+    time = 2.hours.ago
+    report = build(:report, time_2: time)
+    expect(report.time_2).to eq(time)
+  end
+
+  it "has time diff method" do
+    time2 = Time.now
+    time1 = 2.hours.ago
+    report = build(:report, time_1: time1, time_2: time2)
+    expect(report.time_diff).to eq(2)
   end
 end
