885e3f2f830edfd8bbe4246720c07c6d87d650a2
Piet Geursen <pietgeursen@gmail.com>
2015-10-13 10:10:59 +1300

    Add 2 basic tests

--- spec/features/toggle_menus_spec.rb
+++ spec/features/toggle_menus_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
 
 RSpec.feature "ToggleMenus", type: :feature, js: true do
-	describe 'click on toggle menu' do
+	describe 'Toggle menu' do
 		it 'Toggle menu is on the page' do
 			visit root_path
 			expect(page).to have_content('Toggle menu')
+		end
+		it 'Clicking on toggle menu displays the menu' do
+			visit root_path
+			expect(page).not_to have_content('Report')
+			click_on 'Toggle menu'
+			expect(page).to have_content('Report')
 		end
 	end
 end
