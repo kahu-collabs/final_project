bb1136e076fab054d015f97a5aa54d72943923fd
Joshua Vial <joshua@enspiral.com>
2015-10-14 11:14:52 +1300

    removing empty lines

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,6 @@ class SessionsController < ApplicationController
   end
 
   def create
-
     user = User.find_or_create_from_omniauth(request.env['omniauth.auth'])
     p 'user', user
     session[:user_id] = user.id
