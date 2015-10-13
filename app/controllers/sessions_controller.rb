67869b26e2caaf72b7d1b7b0fbaf86fcd1f8a7b9
Joshua Vial <joshua@enspiral.com>
2015-10-14 11:15:08 +1300

    removing debug code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,6 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_or_create_from_omniauth(request.env['omniauth.auth'])
-    p 'user', user
     session[:user_id] = user.id
     redirect_to root_path
   end
